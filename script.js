c11a14445e054523b122f21d286a0654d5bf6c59
Gurmeet Rathor <156289823+gurmeet1011@users.noreply.github.com>
2024-12-12 18:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const form=document.getElementById('simpleForm');
+const nameInput=document.getElementById('name');
+const ageInput=document.getElementById('age');
+
+form.addEventListener('submit',(event)=>{
+	event.preventDefault();
+
+	const name=nameInput.value.trim();
+	const age=parseInt(ageInput.value,10);
+
+	if(!name || isNaN(age))
+	{
+		alert("please fill in all fields.");
+		return;
+	}
+	     const ageValidationPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (age > 18) {
+            resolve();
+          } else {
+            reject();
+          }
+        }, 4000); // 4 seconds delay
+      });
+
+      ageValidationPromise
+        .then(() => {
+          alert(`Welcome, ${name}. You can vote.`);
+        })
+        .catch(() => {
+          alert(`Oh sorry ${name}. You aren't old enough.`);
+        });
+    });
